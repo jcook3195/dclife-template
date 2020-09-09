6fe3c0f2288465c66b6e1797567e659cb15d1a5f
jcook3195 <jcook3195@gmail.com>
2020-09-09 13:13:55 -0400

    add leave a review section

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,7 +1,123 @@
 $(document).ready(function () {
   // init sidenav
-  $('.sidenav').sidenav();
+  if ($('.sidenav').length) {
+    $('.sidenav').sidenav();
+  }
 
   // init carousel
-  $('.carousel').carousel();
+  if ($('.carousel').length) {
+    $('.carousel').carousel();
+  }
+
+  // init modal
+  if ($('.modal').length) {
+    $('.modal').modal();
+  }
+
+  // init tabs
+  if ($('.tabs').length) {
+    $('.tabs').tabs({
+      //swipeable: true,
+    });
+  }
+
+  // review stars
+  $('#star1').on('click', function () {
+    // add star
+    $('#star1-label').html(
+      '<i class="fas fa-star red-text text-darken-1"></i>'
+    );
+    // remove star
+    $('#star2-label').html(
+      '<i class="far fa-star grey-text text-darken-4"></i>'
+    );
+    $('#star3-label').html(
+      '<i class="far fa-star grey-text text-darken-4"></i>'
+    );
+    $('#star4-label').html(
+      '<i class="far fa-star grey-text text-darken-4"></i>'
+    );
+    $('#star5-label').html(
+      '<i class="far fa-star grey-text text-darken-4"></i>'
+    );
+  });
+
+  $('#star2').on('click', function () {
+    // add star
+    $('#star1-label').html(
+      '<i class="fas fa-star red-text text-darken-1"></i>'
+    );
+    $('#star2-label').html(
+      '<i class="fas fa-star red-text text-darken-1"></i>'
+    );
+    // remove star
+    $('#star3-label').html(
+      '<i class="far fa-star grey-text text-darken-4"></i>'
+    );
+    $('#star4-label').html(
+      '<i class="far fa-star grey-text text-darken-4"></i>'
+    );
+    $('#star5-label').html(
+      '<i class="far fa-star grey-text text-darken-4"></i>'
+    );
+  });
+
+  $('#star3').on('click', function () {
+    // add star
+    $('#star1-label').html(
+      '<i class="fas fa-star red-text text-darken-1"></i>'
+    );
+    $('#star2-label').html(
+      '<i class="fas fa-star red-text text-darken-1"></i>'
+    );
+    $('#star3-label').html(
+      '<i class="fas fa-star red-text text-darken-1"></i>'
+    );
+    // remove star
+    $('#star4-label').html(
+      '<i class="far fa-star grey-text text-darken-4"></i>'
+    );
+    $('#star5-label').html(
+      '<i class="far fa-star grey-text text-darken-4"></i>'
+    );
+  });
+
+  $('#star4').on('click', function () {
+    // add star
+    $('#star1-label').html(
+      '<i class="fas fa-star red-text text-darken-1"></i>'
+    );
+    $('#star2-label').html(
+      '<i class="fas fa-star red-text text-darken-1"></i>'
+    );
+    $('#star3-label').html(
+      '<i class="fas fa-star red-text text-darken-1"></i>'
+    );
+    $('#star4-label').html(
+      '<i class="fas fa-star red-text text-darken-1"></i>'
+    );
+    // remove star
+    $('#star5-label').html(
+      '<i class="far fa-star grey-text text-darken-4"></i>'
+    );
+  });
+
+  $('#star5').on('click', function () {
+    // add star
+    $('#star1-label').html(
+      '<i class="fas fa-star red-text text-darken-1"></i>'
+    );
+    $('#star2-label').html(
+      '<i class="fas fa-star red-text text-darken-1"></i>'
+    );
+    $('#star3-label').html(
+      '<i class="fas fa-star red-text text-darken-1"></i>'
+    );
+    $('#star4-label').html(
+      '<i class="fas fa-star red-text text-darken-1"></i>'
+    );
+    $('#star5-label').html(
+      '<i class="fas fa-star red-text text-darken-1"></i>'
+    );
+  });
 });
